eaf62ef96651cf66f0e9f5c891f37100480d5af5
Prince2802 <chaudhary.prince97@gmail.com>
2024-05-31 00:09:18 +0530

    Components in React

--- App.js
+++ App.js
@@ -20,6 +20,43 @@ const heading = React.createElement(
 // JSX - HTML/XML like syntax
 const jsxHeading = <h1 id="Heading">Hello World!!!</h1>
 
+//React Element
+const headingElement = (
+  <h1 className="Heading">Heading as an Element</h1>
+);
+
+// React Component
+// Class based component - OLD
+// Functional Component - NEW
+
+//One way of writing Component's
+const HeadingComponentA = () => {
+  return <h1 className="Heading">This is my Functional Component - 1</h1>
+}
+
+//Second way of writing Component's
+// Component Composition
+const HeadingComponentB = () => (
+  <div>
+    <HeadingComponentA/>
+  <h1 className="Heading">This is my Functional Component - 2</h1>
+    <HeadingComponentC/>
+  </div>
+)
+
+//Third way of writing Component's
+const HeadingComponentC = () => <h1 className="Heading">This is my Functional Component - 3</h1>;
+
+// Functions
+
+//One way of writing fn's
+const fnA = () => true;
+
+//Another way of writing fn's
+const fnB = () => {
+  return true;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
+root.render(<HeadingComponentB/>);
